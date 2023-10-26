865052cfefea7ddde6554491e8781c0e96acc072
focodecided <chinaseeze@gmail.com>
2023-10-25 19:12:22 -0500

    helloworld

---
+++ helloworld.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Script Name:                  Hello World
+# Author:                       Festus Oguhebe Jr. 
+# Date of latest revision:      10/24/2023
+# Purpose:                      Ops201 Challenge 1
+
+# Declaration of variables
+my_variable="Hello World!"
+working_hard="I am working perfectly."
+
+# Declaration of function
+
+# Main
+
+echo $my_variable
+echo $working_hard
+
+
+# End
