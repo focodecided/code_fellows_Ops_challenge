c5d6de7ee0776126077ef632df16b5bf9c9e65a6
focodecided <chinaseeze@gmail.com>
2023-10-25 20:27:37 -0500

    Functions_challenge.sh

---
+++ Functions_challenge.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Script Name:                  Hello World
+# Author:                       Festus Oguhebe Jr. 
+# Date of latest revision:      10/24/2023
+# Purpose:                      Ops201 Challenge 2 - Functions
+
+# Declaration of variables
+
+
+# Declaration of function
+loginhist (g) {
+    last
+    echo "This is the login history"
+}
+# Main
+
+loginhist
+loginhist
+
+
+# End
